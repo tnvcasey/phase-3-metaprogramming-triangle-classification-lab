f9ff950371f4e5c812227a6ba587845bdb1497b9
taylorvercillo <taylornvcasey@gmail.com>
2022-04-23 14:13:17 -0600

    working on it

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1_length, :side_2_length, :side_3_length
+
+  def initialize (side_1_length, side_2_length, side_3_length)
+    @side_1_length = side_1_length
+    @side_2_length = side_2_length
+    @side_3_length = side_3_length
+  end
+
+  def kind
+    if side_1_length == side_2_length && side_3_length
+      return :equilateral 
+    elsif side_1_length == side_2_length != side_3_length
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
 end
